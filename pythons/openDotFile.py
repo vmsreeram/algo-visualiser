100b6ff41d1c067bb9bb008b65d8ab9e7e56e4aa
vmsreeram <112001051@smail.iitpkd.ac.in>
2022-05-21 15:52:14 +0530

    added a new file that converts dot file to png file

--- pythons/openDotFile.py
+++ pythons/openDotFile.py
@@ -1,1 +1,26 @@
-#empty now
+# """
+import pydot
+
+graphs = pydot.graph_from_dot_file('file.dot')
+graph = graphs[0]
+graph.write_png('output.png')
+"""
+import pydot
+
+graph = pydot.Dot("my_graph", graph_type="graph", bgcolor="yellow")
+
+# Add nodes
+my_node = pydot.Node("a", label="Foo")
+graph.add_node(my_node)
+# Or, without using an intermediate variable:
+graph.add_node(pydot.Node("b", shape="circle"))
+
+# Add edges
+my_edge = pydot.Edge("a", "b", color="blue")
+graph.add_edge(my_edge)
+# Or, without using an intermediate variable:
+graph.add_edge(pydot.Edge("b", "c", color="blue"))
+
+graph.write_png("output.png")
+
+"""
