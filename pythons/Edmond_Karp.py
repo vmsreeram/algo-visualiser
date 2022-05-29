09015169a71ab5664b56489e61b84949864e81a2
Saarang <saarang@ideapad.domain.name>
2022-05-29 17:53:18 +0530

    try

--- pythons/Edmond_Karp.py
+++ pythons/Edmond_Karp.py
@@ -8,7 +8,7 @@ frame = Tkinter.Frame(root)
 def max_flow(C, s, t):
         n = len(C) # C is the capacity matrix
         F = [[0] * n for i in range(n)]
-        Edmond_Karp_Vish.makeGraph(C, F, s, t)
+        # Edmond_Karp_Vish.makeGraph(C, F, s, t)
         path = bfs(C, F, s, t)
         print (path)
         while path != None:
