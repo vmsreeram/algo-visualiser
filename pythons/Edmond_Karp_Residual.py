99a718af0e6de043280ac3775f83c44e64f543f5
saarang <112001035@smail.iitpkd.ac.in>
2022-05-30 08:26:27 +0530

    added code to make residual file

---
+++ pythons/Edmond_Karp_Residual.py
@@ -0,0 +1,24 @@
+import pydot
+
+import tkinter as Tkinter
+from PIL import Image, ImageTk
+
+def makeGraph(C, F, s, t):
+    n = len(C)  
+    graph = pydot.Dot("my_graph", graph_type="digraph", bgcolor="yellow")
+
+    # Add nodes
+    for i in range(n):
+        my_node = pydot.Node(str(i))
+        graph.add_node(my_node)
+    graph.get_node(str(s))[0].set_label("source")
+    graph.get_node(str(t))[0].set_label("sink")
+
+    for i in range(n):
+        for j in range(n):
+            if (C[i][j] - F[i][j])>0 :
+                graph.add_edge(pydot.Edge(str(i), str(j), label= str(C[i][j] - F[i][j]) ))
+    # Or, without using an intermediate variable:
+
+    graph.write_png('output1.png')
+    
---
+++ pythons/imageLoad.py
@@ -0,0 +1,61 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+# def destroyFrame(frm):
+#     for widgets in frm.winfo_children():
+#         widgets.destroy()
+
+
+def newImage(win):
+    print("newImage Called")
+    # frame = Frame(win, width=600, height=400)
+    # frame.place(anchor='center', relx=0.5, rely=0.5)
+
+    for widgets in frame.winfo_children():
+        widgets.destroy()
+    img = ImageTk.PhotoImage(Image.open("output.png"))
+
+    # Create a Label Widget to display the text or Image
+    label = Label(frame, image = img)
+    label.pack()
+
+
+
+win = Tk()
+
+# Define the geometry of the window
+win.geometry("700x500")
+
+frame = Frame(win, width=600, height=400)
+frame.place(anchor='center', relx=0.5, rely=0.5)
+
+# newImage(win)
+
+for widgets in frame.winfo_children():
+    widgets.destroy()
+img = ImageTk.PhotoImage(Image.open("output.png"))
+
+# Create a Label Widget to display the text or Image
+label = Label(frame, image = img)
+label.pack()
+
+# Create an object of tkinter ImageTk
+# img = ImageTk.PhotoImage(Image.open("output.png"))
+
+# # Create a Label Widget to display the text or Image
+# label = Label(frame, image = img)
+# label.pack()
+
+# slogan2 = Button(frame,
+#             text="next",
+#             fg="green",
+#             command=win.destroy)
+# slogan2.pack(side=TOP)
+
+slogan2 = Button(frame,
+            text="exit",
+            fg="green",
+            command=win.destroy)
+slogan2.pack(side=TOP)
+
+win.mainloop()
