0272ea35dfb885cfa06124a6c86a97bf9def9a19
vmsreeram <112001051@smail.iitpkd.ac.in>
2022-06-06 11:37:11 +0530

    added retry button, not fully functional

--- Edmond-Karp-Visualisation/ErrorExit.py
+++ Edmond-Karp-Visualisation/ErrorExit.py
@@ -1,12 +1,26 @@
 from tkinter import *
+import os
+# cmd = 'ls -al'
+# os.system(cmd)
+
+def retry():
+    #TODO
+    os.system('python3 start.py')
+    exit()
+    
+    
 
 def raiseErr(errmsg):
+    global win
     errmsg+="!!"
     win=Tk()
     win.geometry("500x300")
     win.title("Error")
-    okBtn=Button(win,text='OK',command=exit,font='times 25')
+
+    okBtn=Button(win,text='Exit',command=exit,font='times 25')
+    # retryBtn=Button(win,text='Retry',command=retry,font='times 25')
     errLbl=Label(win,text=errmsg,font='times 50')
     errLbl.pack()
     okBtn.pack(side=BOTTOM)
+    # retryBtn.pack(side=BOTTOM)
     win.mainloop()
