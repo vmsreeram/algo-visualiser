768b0af5dcad14b70bb48843c48798a43043b739
saarang <112001035@smail.iitpkd.ac.in>
2022-06-16 23:47:48 +0530

    error fixed

--- Dinic's Visualisation/showGraph.py
+++ Dinic's Visualisation/showGraph.py
@@ -3,7 +3,7 @@ import pydot
 def makeGraph(C):
     n = len(C)
 
-    graph = pydot.Dot("my_graph", graph_type="digraph", bgcolor="#204934",fontcolor="white")
+    graph = pydot.Dot("my_graph", graph_type="digraph", bgcolor="#204934",fontcolor="white",sep=3, nodesep=0.9)
 
     # Add nodes
     for i in range(n):
@@ -14,7 +14,7 @@ def makeGraph(C):
     for i in range(n):
         for j in range(n):
             if(C[i][j]>0):
-                graph.add_edge( pydot.Edge(str(i), str(j), label= str(C[i][j]),color = "white",fontsize="20.0",penwidth=1.5) )
+                graph.add_edge( pydot.Edge(str(i), str(j), label= str(C[i][j]),color = "white",fontsize="20.0",penwidth=1.5,fontcolor="orange") )
 
     
     graph.write("levelGraph.png", prog='fdp', format='png')
