fbab777d0f7524d208c2fd351db66f0294be1e71
Arun Sankar <112001008@smail.iitpkd.ac.in>
2022-08-01 17:14:23 +0530

    Added flow/resi label but alignment not proper

--- Dinic's Visualisation/Web-support/display.php
+++ Dinic's Visualisation/Web-support/display.php
@@ -39,6 +39,26 @@
                 height:11%;
                 border: none;
             }
+
+            .anotherlbl{
+                bottom:11%;
+                right:51%;
+                float:right;
+                width:45%;
+                position:absolute;
+                height:11%;
+                border: none;
+            }
+
+            .albl{
+                bottom:11%;
+                left:51%;
+                float:left;
+                width:45%;
+                position:absolute;
+                height:11%;
+                border: none;
+            }
 /* 
             .rightimg{
                 top:10%;
@@ -99,7 +119,8 @@
             $sno=0;
             $imageLft = 'imgs/flow/'.$sno.'.png'; 
             $imageRgt = 'imgs/level/'.$sno.'.png'; 
-
+            $flow = 'imgs/labels/flo.txt';
+            $resi = 'imgs/labels/'.$sno.'.txt'; 
             $target_dir = "imgs/flow/";
             $nlines = count(glob($target_dir . "*"));
             $numLines = intval($nlines);
@@ -114,6 +135,8 @@
         var imageRgt = '<?php echo $imageRgt;?>';
         var labelLft = '<?php echo $labelLft;?>';
         var numLines=parseInt('<?php echo $numLines;?>');
+        var flow = '<?php echo $flow;?>';
+        var resi = '<?php echo $resi;?>';
         function next(){
             // document.getElementById("testing").innerHTML=numLines;
             if(sno+1<numLines)
@@ -134,6 +157,8 @@
             // document.getElementById("testing1").innerHTML=imageLft;
             imageLft='imgs/flow/'+(sno)+'.png'; 
             imageRgt='imgs/level/'+(sno)+'.png'; 
+            flow ='imgs/labels/flo.txt'; 
+            resi = 'imgs/labels/'+(sno)+'.txt'; 
             if(sno!=numLines-1)
             {
                 labelLft= 'imgs/labels/lb.txt'; //Try lb.png instead
@@ -145,6 +170,8 @@
             document.getElementById('lftimgfrm').src=imageLft;
             document.getElementById('rgttimgfrm').src=imageRgt;
             document.getElementById('lftlbl').src=labelLft;
+            document.getElementById('flow').src=flow;
+            document.getElementById('resi').src=resi;
         }
         function prev(){
             if(sno>0)
@@ -165,21 +192,26 @@
             imageLft='imgs/flow/'+(sno)+'.png'; 
             imageRgt='imgs/level/'+(sno)+'.png'; 
             labelLft= 'imgs/labels/lb.txt';
-
+            flow ='imgs/labels/flo.txt'; 
+            resi = 'imgs/labels/'+(sno)+'.txt'; 
             document.getElementById('lftimgfrm').src=imageLft;
             document.getElementById('rgttimgfrm').src=imageRgt;
             document.getElementById('lftlbl').src=labelLft;
+            document.getElementById('flow').src=flow;
+            document.getElementById('resi').src=resi;
         }
         </script>
         <!-- <div> -->
         <button class="buttonl" id="C1" onclick="prev()" hidden> Prev </button>
         <button class="buttonr" id="C2" onclick="next()"> Next </button>
         <!-- </div> -->
-        <!-- <p id="testing"></p>
+        <!-- <p id="testing"></p>leftlbl
         <p id="testing1"></p> -->
         <img id="lftimgfrm" class="leftimg" src="<?php echo $imageLft; ?>">
         <img id="rgttimgfrm" class="rightimg" src="<?php echo $imageRgt; ?>">
         <iframe id="lftlbl" class="leftlbl" src="<?php echo $labelLft; ?>"> </iframe>
+        <iframe id="flow" class="anotherlbl" src="<?php echo $flow; ?>"> </iframe>
+        <iframe id="resi" class="albl" src="<?php echo $resi; ?>"> </iframe>
 
 </body>
 </html>
